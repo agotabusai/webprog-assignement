0a658b1a636ccf3c61604d21db6d4951d8898983
Busai Agota <agota.busai@gmail.com>
2010-04-08 13:07:48 +0200

    second commit

--- app.rb
+++ app.rb
@@ -1,6 +1,15 @@
 require 'rubygems'
 require 'sinatra'
+require 'erb'
 
 get "/" do
-    "<h1>Hello World!</h1>"
+    erb :home
+end
+
+get "/hi" do
+    "<h1>Hello, World!</h1>"
+end
+
+get "/hi/:name" do
+    erb :hi
 end
