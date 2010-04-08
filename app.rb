9754401a30b844e0037958cd99dec58d96959907
Busai Agota <agota.busai@gmail.com>
2010-04-08 17:03:12 +0200

    gyakorlat

--- app.rb
+++ app.rb
@@ -2,10 +2,35 @@ require 'rubygems'
 require 'sinatra'
 require 'erb'
 
+# 8. gyak
+enable :sessions
+
 get "/" do
-    erb :home
+    erb :homelogin
 end
 
+post "/login" do
+    if params[:user]=="bgotti" and params[:pass]=="jelszo"
+	session[:logged_in]=true
+	redirect "/titok"
+    else
+	redirect "/hiba"
+    end
+end
+
+get "/titok" do
+    if session[:logged_in]
+	erb :titok
+    else
+	redirect "/hiba"
+    end
+end
+
+get "/hiba" do
+    erb :hiba
+end
+
+# eddig tart, ami gyakon van
 get "/hi" do
     "<h1>Hello, World!</h1>"
 end
